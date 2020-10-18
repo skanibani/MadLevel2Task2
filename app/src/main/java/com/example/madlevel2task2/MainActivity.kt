b9d78f708c0202ef4d501b217094ec80117479f5
Skander <skander@bibani.nl>
2020-10-18 15:00:31 +0200

    Add answer by swiping behaviour

--- app/src/main/java/com/example/madlevel2task2/MainActivity.kt
+++ app/src/main/java/com/example/madlevel2task2/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.madlevel2task2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.madlevel2task2.databinding.ActivityMainBinding
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,7 +33,51 @@ class MainActivity : AppCompatActivity() {
         for ((key, value) in QuizStatement.QUIZ_STATEMENTS) {
             statements.add(QuizStatement(key, value))
         }
-
         statementAdapter.notifyDataSetChanged()
+
+        binding.rvStatements.addItemDecoration(DividerItemDecoration(this@MainActivity, DividerItemDecoration.VERTICAL))
+        createItemTouchHelper().attachToRecyclerView(rvStatements)
+    }
+
+    private fun checkAnswer(answerPosition: Int, givenAnswer: Boolean): Boolean {
+        return statements[answerPosition].quizAnswer == givenAnswer
+    }
+
+    private fun createItemTouchHelper(): ItemTouchHelper {
+        val callback = object: ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT or ItemTouchHelper.RIGHT) {
+
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                return false
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+
+                val position = viewHolder.adapterPosition
+
+                if (direction == ItemTouchHelper.LEFT) {
+                    // Left swipe answers false
+                    if (checkAnswer(position, false)) {
+                        statements.removeAt(position)
+                        statementAdapter.notifyDataSetChanged()
+                    } else {
+                        Snackbar.make(rvStatements, "Incorrect answer", Snackbar.LENGTH_SHORT).show()
+                    }
+                } else if (direction == ItemTouchHelper.RIGHT) {
+                    // Right swipe answers true
+                    if (checkAnswer(position, true)) {
+                        statements.removeAt(position)
+                        statementAdapter.notifyDataSetChanged()
+                    } else {
+                        Snackbar.make(rvStatements, "Incorrect answer", Snackbar.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+
+        return ItemTouchHelper(callback)
     }
 }
--- app/src/main/java/com/example/madlevel2task2/QuizStatement.kt
+++ app/src/main/java/com/example/madlevel2task2/QuizStatement.kt
@@ -8,7 +8,10 @@ data class QuizStatement(
         val QUIZ_STATEMENTS = mapOf<String, Boolean>(
             "Apples are never green" to false,
             "It never rains in the Netherlands" to false,
-            "King Willem Alexander has red hair" to true
+            "King Willem Alexander has red hair" to true,
+            "The moon spins around its axis faster than the earth" to false,
+            "Apple's iOS runs secretly on Android" to false,
+            "Grass is green, just like Android" to true
         )
     }
 }
